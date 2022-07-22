45af28739eaa743983555b6c0a4353047da1ab1f
Cristian <cris18002011@hotmail.com>
2022-07-22 11:03:11 -0500

    Form validation tests

--- src/components/FromProduct.jsx
+++ src/components/FromProduct.jsx
@@ -1,22 +1,73 @@
 import { useRef } from 'react';
+// import { ProductSchema } from './Validator';
+import { useForm } from 'react-hook-form';
+import * as Joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
+
+// type Inputs = {
+//   title: string,
+//   price: string,
+//   category: number,
+//   description: string,
+//   images: FileList,
+// };
+
+const imageExtentions = ['image/png', 'image/jpg', 'image/jpeg'];
+
+const ValidationSchema = Joi.object({
+  title: Joi.string().min(5).required(),
+  price: Joi.number().greater(0).precision(2).required(),
+  category: Joi.string().valid('1', '2', '3', '4', '5').required(),
+  description: Joi.string().required(),
+  images: Joi.any()
+    .custom(function (file, { error }) {
+      if (!file[0]) {
+        return error('file.required');
+      }
+
+      if (!imageExtentions.includes(file[0].type)) {
+        return error('file.invalid');
+      }
+
+      return file;
+    })
+    .messages({
+      'file.required': 'File is required',
+      'file.invalid': 'The file must be one of the following png, jpg or jpeg.',
+    }),
+});
 
 export default function FormProduct() {
   const formRef = useRef(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(formRef.current);
-    const data = {
-      title: formData.get('title'),
-      price: parseInt(formData.get('price')),
-      description: formData.get('description'),
-      categoryId: parseInt(formData.get('category')),
-      images: [formData.get('images').name],
-    };
-    console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: joiResolver(ValidationSchema),
+  });
+
+  const onSubmit = (d) => {
+    console.log(d);
   };
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(formRef.current);
+  //   const data = {
+  //     title: formData.get('title'),
+  //     price: parseInt(formData.get('price')),
+  //     description: formData.get('description'),
+  //     categoryId: parseInt(formData.get('category')),
+  //     images: [formData.get('images').name],
+  //   };
+  //   console.log(data);
+  // const validation = await ProductSchema.validate(data);
+  // console.log({ validation });
+  //};
   return (
-    <form ref={formRef} onSubmit={handleSubmit}>
+    <form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
       <div className="overflow-hidden">
         <div className="px-4 py-5 bg-white sm:p-6">
           <div className="grid grid-cols-6 gap-6">
@@ -24,13 +75,27 @@ export default function FormProduct() {
               <label htmlFor="title" className="block text-sm font-medium text-gray-800">
                 Title
               </label>
-              <input type="text" name="title" id="title" className="mt-1 focus:ring-blue-800 focus:border-blue-800 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <input
+                type="text"
+                name="title"
+                id="title"
+                className={`mt-1 focus:ring-blue-800 focus:border-blue-800 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md ${errors.title && 'border-red-300'}`}
+                {...register('title')}
+              />
+              {errors.title && <span className="text-red-400 font-thin text-sm">{errors.title.message}</span>}
             </div>
             <div className="col-span-6 sm:col-span-3">
               <label htmlFor="price" className="block text-sm font-medium text-gray-700">
                 Price
               </label>
-              <input type="number" name="price" id="price" className="mt-1 focus:ring-blue-800 focus:border-blue-800 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <input
+                type="number"
+                {...register('price')}
+                name="price"
+                id="price"
+                className={`mt-1 focus:ring-blue-800 focus:border-blue-800 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md ${errors.price && 'border-red-300'}`}
+              />
+              {errors.price && <span className="text-red-400 font-thin text-sm">{errors.price.message}</span>}
             </div>
             <div className="col-span-6">
               <label htmlFor="category" className="block text-sm font-medium text-gray-700">
@@ -39,6 +104,7 @@ export default function FormProduct() {
               <select
                 id="category"
                 name="category"
+                {...register('category')}
                 autoComplete="category-name"
                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-800 focus:border-blue-800 sm:text-sm"
               >
@@ -57,14 +123,18 @@ export default function FormProduct() {
               <textarea
                 name="description"
                 id="description"
+                {...register('description')}
                 autoComplete="description"
                 rows="3"
-                className="form-textarea mt-1 block w-full focus:ring-blue-800 focus:border-blue-800 shadow-sm sm:text-sm border-gray-300 rounded-md"
+                className={`form-textarea mt-1 block w-full focus:ring-blue-800 focus:border-blue-800 shadow-sm sm:text-sm border-gray-300 rounded-md ${errors.description && 'border-red-300'}`}
               />
+              {errors.description && <span className="text-red-400 font-thin text-sm">{errors.description.message}</span>}
             </div>
             <div className="col-span-6">
               <div>
-                <label className="block text-sm font-medium text-gray-700">Cover photo</label>
+                <label htmlFor="images" className="block text-sm font-medium text-gray-700">
+                  Cover photo
+                </label>
                 <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                   <div className="space-y-1 text-center">
                     <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
@@ -81,13 +151,14 @@ export default function FormProduct() {
                         className="relative cursor-pointer bg-white rounded-md font-medium text-blue-800 hover:text-blue-900 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-800"
                       >
                         <span>Upload a file</span>
-                        <input id="images" name="images" type="file" className="sr-only" />
+                        <input id="images" name="images" {...register('images')} type="file" className="sr-only" />
                       </label>
                       <p className="pl-1">or drag and drop</p>
                     </div>
                     <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                   </div>
                 </div>
+                {errors.images && <span className="text-red-400 font-thin text-sm text-center w-full">{errors.images.message}</span>}
               </div>
             </div>
           </div>
